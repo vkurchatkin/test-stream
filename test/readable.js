c5828103cd4b66c4d944fcc12d101d7f092d5c00
Vladimir Kurchatkin <vladimir.kurchatkin@gmail.com>
2014-02-11 14:13:55 +0400

    shortcut for arrays

--- test/readable.js
+++ test/readable.js
@@ -3,7 +3,8 @@ require('should')
 var Readable = require('./..').Readable
 var Writable = require('./..').Writable
 
-var buffer = new Buffer([0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09])
+var bytes = [0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09]
+var buffer = new Buffer(bytes)
 
 describe('Readable', function () {
   it('should work as a function', function () {
@@ -12,6 +13,20 @@ describe('Readable', function () {
 
   it('should stream buffer', function (done) {
     var rs = new Readable(buffer)
+    var ws = new Writable
+
+    ws.on('finish', onFinish)
+
+    rs.pipe(ws)
+
+    function onFinish () {
+      ws.toBuffer().should.eql(buffer)
+      done()
+    }
+  })
+
+  it('should work with array', function (done) {
+    var rs = new Readable(bytes)
     var ws = new Writable
 
     ws.on('finish', onFinish)
